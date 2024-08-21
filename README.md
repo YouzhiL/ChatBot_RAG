5c561a0524379ee17c7eb397f19060e30c2fc8c9
Youzhi <youzhi797@gmail.com>
2024-08-21 17:43:49 -0600

    add tracing and evaluation framework

--- README.md
+++ README.md
@@ -7,6 +7,45 @@ Implement a chatbot augmented with customized database.
 
 ## Run
 
-1. Modify `.env_example` by adding your `OPENAI_API_KEY` and `LLAMA_CLOUD_API_KEY`. Change the file name to `.env`
+1. Modify `.env_example` by adding your `OPENAI_API_KEY`,`ANTHROPIC_API_KEY` and `LLAMA_CLOUD_API_KEY`. Change the file name to `.env`
 2. Add PDF files to ./data folder (feel free to keep/delete the existing files)
 3. Run base parser using ```python cli.py base_parse data/[file_1.pdf] data/[file_n.pdf]```
+
+
+## Tracing & Evaluate RAG workflow
+- We trace and evaluate the RAG application by using the Phoenix framework. 
+- Use URL `http://localhost:6006/` to get a decomposed view of the query engine operation.
+- Evaluation Standard: Retrieval quality; Generation quality; Faithfulness; Efficiency; Robustness
+- Phoenix as the evaluation framework: use Chat GPT4 as a reference to decide on the correctness of RAG's answer
+- [How it works](https://docs.arize.com/phoenix/evaluation/concepts-evals/llm-as-a-judge)
+- Intermediate result is also stored in folder `intermediate result`.
+## Discussion - BaseParse Mode
+1. File Loader
+- `SimpleDirectoryReader`: able to adapt to different file types and select tools to extract the content based on the file extensions. For binary files such as PDF and Offices docs, it uses PyPDF and Pillow.
+
+2. Transfomer
+- `TokenTextSplitter`: Split the Document text into chunks that contain whole sentences.
+- `SummaryExtractor`: Generate Summaries of the text contained by the node. (claude-3-opus-20240229 is slow in this task)
+- `OpenAIEmbedding`: deault model trained to produce embeddings that effectively capture semantic meanings of the text.
+
+2. Build Index
+`VectorStoreIndex`: The workhorse in most RAG applications. Used for efficient querying because it allows for similarity searches over the embedded representations of the text.
+- Similarity Search: When a query is made, the query text will be embedded and compared against the stored vectors using a similarity measure identified with cosine similarity. Cosine similarity measures the cosine of the angle between two vectors. The smaller the angle between them, the more similar they are.
+- Local embedding: a well-balanced default model provided by Hugging Face.
+
+`TreeIndex`: Generated bt LLM. Useful for summarization. (not used in base parser)
+
+3. Store Index (TO-DO)
+- Local store: persist and load from disk.
+- Vector Database: Picone, CheomaDB, etc.
+
+
+4. Build QueryEngine
+QueryEngine is an interface that process natural language queries to generate rich responses. It often relies on one or more indexes through retrievers and can also be combined with other query engines for enhanced capabilities.
+- high-level API
+
+- low-level API
+
+`VectorIndexRetriever`: 
+
+`ResponseSynthesizer`: Generate responses from a language model using a user query and the retrieved context.
--- base_parser.py
+++ base_parser.py
@@ -1,6 +1,7 @@
 from llama_index.llms.openai import OpenAI
+from llama_index.llms.anthropic import Anthropic
 from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext,load_index_from_storage, get_response_synthesizer, set_global_handler
 from llama_index.core import Settings
 from llama_index.core.node_parser import TokenTextSplitter,MarkdownElementNodeParser
 from llama_index.core.extractors import SummaryExtractor
@@ -9,8 +10,12 @@ from llama_index.core.ingestion import IngestionPipeline, IngestionCache
 from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.core.storage.chat_store import SimpleChatStore
 from llama_index.agent.openai import OpenAIAgent
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.postprocessor import SimilarityPostprocessor
+from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from dotenv import load_dotenv
+import phoenix as px
 
 
 from global_setting import PIPELINE_CACHE, INDEX_STORAGE, CONVERSATION_FILE
@@ -23,15 +28,25 @@ def run_base_parse(file_paths):
   load_dotenv()
   embed_model = OpenAIEmbedding(model="text-embedding-3-small")
   llm = OpenAI(model="gpt-4o")
-  # print(file_names)
+  # llm = Anthropic(model="claude-3-opus-20240229")
+
+  ### start the Phoenix server
+  px.launch_app()
+  set_global_handler("arize_phoenix")
+  
   ### Load documents
   documents = SimpleDirectoryReader(input_files=file_paths, filename_as_id=True).load_data()
+  with open("doc/"+"extractedDoc.txt","w") as file:
+    for doc in documents:
+      file.write(doc.get_content())
   print(f"Finish loading files")
+  
   ### Build nodes
   try:
     cached_hashes = IngestionCache.from_persist_path(PIPELINE_CACHE)
   except:
     cached_hashes = None
+    
   pipeline = IngestionPipeline(
     transformations=[
       TokenTextSplitter(chunk_size=1024, chunk_overlap=20),
@@ -44,6 +59,9 @@ def run_base_parse(file_paths):
   nodes = pipeline.run(documents=documents)
   pipeline.persist(PIPELINE_CACHE)
   print("Finish building nodes")
+  
+  
+  
   ### Build index
   
   # try:
@@ -81,6 +99,8 @@ def run_base_parse(file_paths):
     persist_dir=INDEX_STORAGE
   )
   print("New indexes created and persisted.")
+  
+  
   ### Initialize chatbot
   try:
     chat_store = SimpleChatStore.from_persist_path(CONVERSATION_FILE)
@@ -91,11 +111,26 @@ def run_base_parse(file_paths):
   ### Build query engine 
   storage_context = StorageContext.from_defaults(persist_dir=INDEX_STORAGE)
   index = load_index_from_storage(storage_context=storage_context, index_id="vector")
-  query_engine = index.as_query_engine(similarity_top_k=3)
+  
+  # high-level API
+  # query_engine = index.as_query_engine(similarity_top_k=3) # use RetrieverQueryEngine under the hood
+  
+  # low-level API - assemble a query engine by retriever, response_synthesizer
+  retriever = VectorIndexRetriever(index=index, similarity_top_k=3)
+  response_synthesizer = get_response_synthesizer(response_mode="compact", verbose=True)
+  post_processor = SimilarityPostprocessor(similarity_cutoff=0.7)
+  
+  query_engine = RetrieverQueryEngine(
+    retriever=retriever,
+    response_synthesizer=response_synthesizer,
+    node_postprocessors=[post_processor],
+  )
+  
   metadata=ToolMetadata(
     name="information_retriever",
     description="Provides official information about the document. Use a detailed plain text auestion as input to the tool"
   )
+  
   query_engine_tool = QueryEngineTool(
     query_engine=query_engine,
     metadata=metadata
@@ -107,7 +142,7 @@ def run_base_parse(file_paths):
     tools=[query_engine_tool],
     llm=llm,
     memory=memory,
-    system_prompt= "You are an expert of information retriver. When asked a question, you are able to find the answer in the documents provided."
+    system_prompt= "You are an expert of information retriever. When asked a question, you are able to find the answer in the documents provided."
   )
   
   # doc_parser = LlamaParse(result_type="markdown", verbose=True)
@@ -127,6 +162,56 @@ def run_base_parse(file_paths):
     response = str(agent.chat(prompt))
     print(response)
     prompt = input("Please enter the question. Enter q to exit: \n")
-  
+
+  ### EVALUATION PART 
+  # adapted from the examples available on the official Phoenix documentation: https://docs.arize.com/phoenix/
+
+  from phoenix.session.evaluation import (
+      get_qa_with_reference, 
+      get_retrieved_documents
+  )
+  from phoenix.trace import DocumentEvaluations, SpanEvaluations
+  from phoenix.evals import (
+      HallucinationEvaluator,
+      QAEvaluator,
+      RelevanceEvaluator,
+      OpenAIModel,
+      run_evals
+  )
+
+
+  model = OpenAIModel(model="gpt-4o")
+
+  retrieved_documents_df = get_retrieved_documents(px.Client())
+  queries_df = get_qa_with_reference(px.Client())
+
+  hallucination_evaluator = HallucinationEvaluator(model)
+  qa_correctness_evaluator = QAEvaluator(model)
+  relevance_evaluator = RelevanceEvaluator(model)
+
+  hallucination_eval_df, qa_correctness_eval_df = run_evals(
+      dataframe=queries_df,
+      evaluators=[hallucination_evaluator, qa_correctness_evaluator],
+      provide_explanation=True,
+  )
+  relevance_eval_df = run_evals(
+      dataframe=retrieved_documents_df,
+      evaluators=[relevance_evaluator],
+      provide_explanation=True,
+  )[0]
+
+  px.Client().log_evaluations(
+      SpanEvaluations(
+          eval_name="Hallucination", 
+          dataframe=hallucination_eval_df),
+      SpanEvaluations(
+          eval_name="QA Correctness", 
+          dataframe=qa_correctness_eval_df),
+      DocumentEvaluations(
+          eval_name="Relevance", 
+          dataframe=relevance_eval_df),
+  )
+  input("Press <ENTER> to exit")
+
 if __name__ == "__main__":
-  base_parse("n_beats.pdf")
+  run_base_parse("n_beats.pdf")
